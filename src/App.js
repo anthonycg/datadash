79571a32132ce035f4763be334cb754fa950a58b
anthonycg <anthonygibson24@gmail.com>
2024-11-02 20:17:39 -0500

    update coords on location selection

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import WeatherChart from "./WeatherChart";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
-import LocationSelector from "./LocationSelector";
 
 Chart.register(CategoryScale);
 
@@ -12,6 +11,7 @@ function App() {
     const [apiData, setApiData] = useState([]); // Change to an array
     const [loading, setLoading] = useState(true); // For loading state
     const [error, setError] = useState(null); // For error state
+    const [coords, setCoords] = useState({ lat: 25.7743, lon: -80.1937 }); // Location Coordinates - MIA by default
 
     useEffect(() => {
         const fetchWeatherData = async () => {
@@ -19,8 +19,8 @@ function App() {
                 const response = await axios.post(
                     "http://127.0.0.1:8000/getWeather",
                     {
-                        lat: 25.7743, // Miami
-                        lon: -80.1937,
+                        lat: coords.lat,
+                        lon: coords.lon,
                     },
                     {
                         headers: {
@@ -31,7 +31,7 @@ function App() {
                     }
                 );
                 console.log(JSON.parse(response.data.data.body));
-                setApiData(JSON.parse(response.data.data.body)); // Wrap the response in an array
+                setApiData(JSON.parse(response.data.data.body));
             } catch (error) {
                 console.error("Error fetching weather data:", error);
                 setError("Error fetching weather data");
@@ -41,7 +41,7 @@ function App() {
         };
 
         fetchWeatherData();
-    }, []);
+    }, [coords]);
 
     // Handle loading and error states
     if (loading) return <div>Loading...</div>;
@@ -49,7 +49,36 @@ function App() {
 
     return (
         <div>
-            <LocationSelector />
+            <div className="mt-2">
+                <button
+                    class="bg-pink-700 text-cyan-800 text-xl rounded-md p-2 mx-2"
+                    onClick={() => setCoords({ lat: 25.7743, lon: -80.1937 })}
+                >
+                    Miami, FL
+                </button>
+                {/* 25.7743, lon: -80.1937 */}
+                <button
+                    class="bg-cyan-950 text-amber-400 text-xl rounded-md p-2 mr-2"
+                    onClick={() => setCoords({ lat: 35.2339, lon: -75.5364 })}
+                >
+                    Cape Hatteras, NC
+                </button>
+                {/* 35.2339, lon: -75.5364 */}
+                <button
+                    class="bg-purple-700 text-amber-50 text-xl rounded-md p-2 mr-2"
+                    onClick={() => setCoords({ lat: 29.7633, lon: -95.3633 })}
+                >
+                    Houston, TX
+                </button>
+                {/* 29.7633, lon: -95.3633 */}
+                <button
+                    class="bg-yellow-900 text-white text-xl rounded-md p-2 mr-2"
+                    onClick={() => setCoords({ lat: 29.9547, lon: -90.0751 })}
+                >
+                    New Orleans, LA
+                </button>
+                {/* 29.9547 -90.0751 */}
+            </div>
             <WeatherChart data={apiData} />
         </div>
     );
